64cdd2e7ae8ac993880cb8741188bf567c5aa6c3
https://github.com/HurasAdam <huradam@wp.pl>
2024-11-24 20:25:32 +0100

    feat: Added date filter to getUsersWithReportCountHandler for report counts within a date range

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -20,19 +20,47 @@ export const getUserHandler = catchErrors(async (req, res) => {
 
 
   export const getUsersWithReportCountHandler = catchErrors(async (req, res) => {
-    // 1. Użyj agregacji w MongoDB, aby policzyć liczbę raportów dla każdego użytkownika
+    // Pobierz daty z zapytania jako string
+    const { startDate, endDate } = req.query;
+  
+    // Przygotuj zakres dat (jeśli podano), konwertując wartości na Date
+    let dateFilter: any = {};  // Typowanie jako "any", ponieważ jest to obiekt z zapytaniem MongoDB
+  
+    // Sprawdzamy, czy startDate lub endDate są dostępne
+    if (startDate || endDate) {
+      dateFilter = { createdAt: {} };  // Filtrujemy po polu createdAt
+  
+      // Sprawdzamy, czy startDate jest prawidłowy
+      if (startDate) {
+        const start = new Date(startDate as string);  // Konwertujemy na Date
+        if (!isNaN(start.getTime())) {  // Sprawdzamy, czy data jest prawidłowa
+          dateFilter.createdAt.$gte = start;  // Data początkowa
+        }
+      }
+  
+      // Sprawdzamy, czy endDate jest prawidłowy
+      if (endDate) {
+        const end = new Date(endDate as string);  // Konwertujemy na Date
+        if (!isNaN(end.getTime())) {  // Sprawdzamy, czy data jest prawidłowa
+          dateFilter.createdAt.$lte = end;  // Data końcowa
+        }
+      }
+    }
+  
+    // 1. Użyj agregacji w MongoDB, aby policzyć liczbę raportów dla każdego użytkownika w danym zakresie dat
     const usersWithReportCount = await ConversationReportModel.aggregate([
-      // Krok 1: Zgrupuj raporty po użytkowniku i policz ich liczbę
+      {
+        $match: dateFilter, // Filtruj raporty po dacie, jeśli podano
+      },
       {
         $group: {
           _id: "$createdBy", // Grupujemy po użytkowniku
           reportCount: { $sum: 1 }, // Zliczamy raporty
         },
       },
-      // Krok 2: Połącz z danymi użytkownika (populate)
       {
         $lookup: {
-          from: "users", // Kolekcja użytkowników
+          from: "users",
           localField: "_id", // Pole, które łączy (ID użytkownika)
           foreignField: "_id", // Pole w kolekcji użytkowników
           as: "user", // Zapisz połączone dane użytkownika w polu "user"
@@ -43,13 +71,12 @@ export const getUserHandler = catchErrors(async (req, res) => {
       },
       {
         $project: {
-          _id: "$user._id",
+          _id: { $toObjectId: "$user._id" }, // Rzutowanie na ObjectId
           name: "$user.name",
           surname: "$user.surname",
           reportCount: 1, // Liczba raportów
         },
       },
-      // Krok 3: Posortuj użytkowników po liczbie raportów w malejącej kolejności
       {
         $sort: {
           reportCount: -1, // Malejąco
@@ -61,7 +88,7 @@ export const getUserHandler = catchErrors(async (req, res) => {
     const allUsers = await UserModel.find();
   
     // 3. Połącz użytkowników z agregacji z użytkownikami bez raportów
-    const usersWithZeroReports = allUsers.filter(user => 
+    const usersWithZeroReports = allUsers.filter(user =>
       !usersWithReportCount.some(report => report._id.toString() === user._id.toString())
     ).map(user => ({
       _id: user._id,
@@ -79,3 +106,4 @@ export const getUserHandler = catchErrors(async (req, res) => {
     // 5. Zwróć posortowaną listę użytkowników
     return res.status(OK).json(allUsersWithReportCounts);
   });
+  
