57a482df6ba6c2eed8b407a4716f43191ce531ac
https://github.com/HurasAdam <huradam@wp.pl>
2024-11-30 23:46:48 +0100

    Refactor: Update ShortcutCallRegisterForm to dynamically display conversation topics with ConversationReportCard

--- frontend/src/components/forms/ShortcutCallRegisterForm.tsx
+++ frontend/src/components/forms/ShortcutCallRegisterForm.tsx
@@ -8,19 +8,17 @@ import {ErrorMessage} from "@hookform/error-message";
 import { SelectBox } from "../core/SelectBox";
 import { useQuery } from "@tanstack/react-query";
 import { conversationTopicApi } from "@/lib/conversationTopicsApi";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { ComboboxDemo } from "../core/ComboBox";
+import ConversationReportCard from "../ConversationReportCard";
 
 
 
 
 
 interface Inputs {
-    name: string;
-    password: string;
-    password_confirmation: string;
-    email: string;
-    age: string;
-    phone_number: string;
-    city:string;
+    conversationTopic: string;
+
 }
 
 
@@ -41,10 +39,14 @@ export default function ShortcutCallRegisterForm({setActualStep, setFullFormData
 
 
     const maxDate = new Date().toISOString().split("T")[0]
-    const {register, handleSubmit, formState, getValues} = useForm<Inputs>()
+    const {register, handleSubmit, formState, getValues,watch} = useForm<Inputs>({
+        defaultValues:{
+            conversationTopic:""
+        }
+    })
     const {errors} = formState
 
-
+    const selectedTopic = watch("conversationTopic"); // Obserwuj wartość pola
 
    
 
@@ -58,47 +60,12 @@ export default function ShortcutCallRegisterForm({setActualStep, setFullFormData
     
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6 py-5 px-2">
-    
-            
-            <div className="grid sm:grid-flow-col gap-6">
-                <div className="grid w-full items-center gap-1.5">
-                <SelectBox 
-data={formatedConversationTopcis}
-// data={formatedAuthors}
-value="Promocja słuchaczy"
-label="Autor"
-/>
-                    <ErrorMessage name="name" errors={errors}
-                                  render={({message}) => <p className="text-red-500 text-sm">{message}</p>}/>
-                </div>
-            </div>
-            <div className="grid sm:grid-flow-col gap-6">
-                <div className="grid w-full items-center gap-1.5">
-                    <Label htmlFor="email">Email</Label>
-                    <Input  id="email" type="email" {...register("email", {
-                        required: "Uzupełnij adres email", pattern: {
-                            value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                            message: 'Wprowadź poprawny adres email',
-                        },
-                    })}/>
-                    <ErrorMessage name="email" errors={errors}
-                                  render={({message}) => <p className="text-red-500 text-sm">{message}</p>}/>
-                </div>
-            </div>
-      
-   
-    
-       
-    
-          
-            <Button
-            
-              >
-    
-    Szukaj
-            </Button>
- 
-        </form>
+        <div className='flex flex-col gap-3.5'>
+        {conversationTopics?.map((topic:ITopic) => {
+          return (
+  <ConversationReportCard topic={topic}/>
+          )
+        })}
+        </div>
     )
 }
