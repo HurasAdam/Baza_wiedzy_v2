1d6a26182b09f9b4d18f69550561e7c4aa5f3b08
HurasAdam <huradam@wp.pl>
2024-12-09 12:14:32 +0100

    feat: update AuthLayout component with new styles image.

--- frontend/src/constants/images.ts
+++ frontend/src/constants/images.ts
@@ -1,9 +1,10 @@
 import Logo from "../../../frontend/public/logo.png";
 import data_placeholder from "../../../frontend/public/no-data-placeholder.png";
 import adminImage  from "../../public/adminPanel-image.png";
-
+import loginImage from "../../public/loginImage.png";
 export const IMAGES ={
     Logo,
     data_placeholder,
-    adminImage
+    adminImage,
+    loginImage
 }
--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,3 +1,4 @@
+import { IMAGES } from '@/constants/images';
 import useAuth from '@/hooks/useAuth';
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
@@ -12,9 +13,13 @@ if(user){
   return <Navigate to="/dashboard"/>
 }else{
   return (
-    <section className='min-h-screen flex w-full bg-customDark'>
-      <div className='flex-1 my-auto hidden lg:flex flex-col items-center mt-20'>
-        <h1 className=' text-5xl text-neutral-100 font-semibold font-inter'>Baza wiedzy</h1>
+    <section className='min-h-screen flex w-full bg-blue-400'>
+      <div className='flex-1 my-auto hidden lg:flex flex-col items-center mt-10'>
+      <span className='text-orange-700 font-bold text-2xl mb-1.5'>LIBRUS</span>
+        <h1 className=' text-6xl text-blue-50 font-bold font-inter '>Baza wiedzy</h1>
+        <span className='my-2 text-indigo-200 font-semibold'>Helpdesk</span>
+      
+        <img src={IMAGES.loginImage} alt="" />
 {/* <img className='w-auto max-h-[700px]' src={CONSTANTS.IMAGES.landingImage} alt="" /> */}
 
 </div>
