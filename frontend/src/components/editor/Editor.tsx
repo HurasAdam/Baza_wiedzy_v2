7e50a558bee6fd0330cc938f116e5c9ee638616c
https://github.com/HurasAdam <huradam@wp.pl>
2024-12-21 22:59:11 +0100

    feat: Added rich text editor using ReactQuill

---
+++ frontend/src/components/editor/Editor.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"
+const Editor = ({style, className,onChange, ...field  }) => {
+
+    const handleEditorChange = (content, delta, source, editor) => {
+        // Usuwamy pustą treść, jeśli jest
+        if (content === "<p><br></p>") {
+          onChange('');
+        } else {
+          onChange(content);
+        }
+      };
+
+
+  return (
+   <ReactQuill 
+   onChange={handleEditorChange}
+   style={{
+    ...style,
+   
+    width: '100%'  // Zapewnia, że edytor zajmuje całą szerokość rodzica (modalu)
+  }}
+   className={`ql-container ${className}`}  // Dodajemy klasy dla kontenera
+   theme='snow' {...field}/>
+  )
+}
+
+export default Editor
