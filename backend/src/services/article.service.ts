c6d1882e1d2064429745cc69b122f59ee0c01493
HurasAdam <huradam@wp.pl>
2024-12-24 23:26:51 +0100

    feat: updated getArticle function to include 'verifiedBy' field in population.

--- backend/src/services/article.service.ts
+++ backend/src/services/article.service.ts
@@ -3,57 +3,56 @@ import ArticleModel from "../models/Article.model";
 import UserModel from "../models/User.model";
 import appAssert from "../utils/appAssert";
 
-
 interface CreateArticleRequest {
-    title: string;
-    employeeDescription: string;
-    tags: string[];
-    clientDescription: string;
-  }
-  
-  interface CreateArticleParams {
-    request: CreateArticleRequest;
-    userId: string; // Zakładam, że userId to string
-  }
+  title: string;
+  employeeDescription: string;
+  tags: string[];
+  clientDescription: string;
+}
 
+interface CreateArticleParams {
+  request: CreateArticleRequest;
+  userId: string; // Zakładam, że userId to string
+}
 
-export const createArticle = async({request,userId}:CreateArticleParams)=>{
-const {title,employeeDescription,tags,clientDescription} = request;
+export const createArticle = async ({
+  request,
+  userId,
+}: CreateArticleParams) => {
+  const { title, employeeDescription, tags, clientDescription } = request;
 
-const article = await ArticleModel.exists({ title });
-appAssert(!article, CONFLICT, "Article already exists");
+  const article = await ArticleModel.exists({ title });
+  appAssert(!article, CONFLICT, "Article already exists");
 
-console.log("request");
-console.log(request);
+  console.log("request");
+  console.log(request);
 
-const createdArticle = await ArticleModel.create({
+  const createdArticle = await ArticleModel.create({
     title,
     employeeDescription,
     clientDescription,
     tags,
-    createdBy:userId,
-    verifiedBy:userId
-})
-return createdArticle
+    createdBy: userId,
+    verifiedBy: userId,
+  });
+  return createdArticle;
 };
 
-
-
-// Article 
-interface getArticleParams{
-  userId:string;
-  articleId:string;
+// Article
+interface getArticleParams {
+  userId: string;
+  articleId: string;
 }
 
-export const getArticle = async({userId,articleId}:getArticleParams)=>{
-
-const user = await UserModel.findById(userId);
+export const getArticle = async ({ userId, articleId }: getArticleParams) => {
+  const user = await UserModel.findById(userId);
   const article = await ArticleModel.findById({ _id: articleId }).populate([
-    {path:"tags", select:["name"]},
-    {path:"createdBy", select:["name","surname"]},
-  ])
+    { path: "tags", select: ["name"] },
+    { path: "createdBy", select: ["name", "surname"] },
+    { path: "verifiedBy", select: ["name", "surname"] },
+  ]);
 
-  console.log(user)
+  console.log(user);
 
   appAssert(article, CONFLICT, "Article not found");
   appAssert(user, CONFLICT, "User not found");
@@ -61,16 +60,14 @@ const user = await UserModel.findById(userId);
   const isFavorite = user?.favourites.includes(article._id);
 
   const articleObj = {
-    ...article.toObject(), 
-    isFavourite: isFavorite || false, 
+    ...article.toObject(),
+    isFavourite: isFavorite || false,
   };
 
   return articleObj;
-}
-
+};
 
 // ARTICLES
-
 
 // interface GetArticlesOptions {
 //     title?: string;
@@ -81,34 +78,32 @@ const user = await UserModel.findById(userId);
 //     page: number;
 //     sortBy: string;
 //   }
-  
 
-  
 //   export const getAllArticles = async ({options,req}:{options:GetArticlesOptions,req:Express.Request} ) => {
 //     const { title, tags, author, verified, limit, page, sortBy } = options;
-    
+
 //     const query: any = {};
-  
+
 //     // Konstruowanie zapytania
 //     if (title) {
 //       query.title = new RegExp(title, "i");
 //     }
-  
+
 //     if (tags) {
 //       query.tags = { $all: tags };
 //     }
-  
+
 //     if (author) {
 //       query.createdBy = author;
 //     }
-  
+
 //     if (verified !== undefined) {
 //       query.isVerified = verified;
 //     }
-  
+
 //     const pageSize = limit;
 //     const skipp = (page - 1) * pageSize;
-  
+
 //     const articles = await ArticleModel.find(query)
 //       .select([
 //         "-clientDescription",
@@ -125,9 +120,9 @@ const user = await UserModel.findById(userId);
 //       .skip(skipp)
 //       .limit(pageSize)
 //       .sort(sortBy);
-  
+
 //     const total = await ArticleModel.countDocuments(query);
-  
+
 //     return {
 //       articles,
 //       total,
