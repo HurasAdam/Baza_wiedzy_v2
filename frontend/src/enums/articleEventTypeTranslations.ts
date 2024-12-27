84e38c5fa969303a0d24b6d923abfa87afd24497
HurasAdam <huradam@wp.pl>
2024-12-27 12:51:11 +0100

    feat: Added event type translations for frontend.

---
+++ frontend/src/enums/articleEventTypeTranslations.ts
@@ -0,0 +1,9 @@
+const EVENT_TYPE_TRANSLATIONS = {
+  commented: "Skomentowano",
+  created: "Utworzono",
+  verified: "Zweryfikowano",
+  trashed: "Usunięto",
+  updated: "Zaktualizowano",
+};
+
+export default EVENT_TYPE_TRANSLATIONS;
