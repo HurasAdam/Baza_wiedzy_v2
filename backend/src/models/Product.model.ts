e58a2183be8d7f9285fcd62e103496d4ca02d6cc
HurasAdam <huradam@wp.pl>
2025-01-03 22:44:28 +0100

    fea: Added 'banner' field with predefined enum to Product model.

--- backend/src/models/Product.model.ts
+++ backend/src/models/Product.model.ts
@@ -1,10 +1,23 @@
-import { Schema,model } from "mongoose";
+import { Schema, model } from "mongoose";
 
-const productSchema= new Schema({
-    name:{type:String, required:true},
-    createdBy:{type: Schema.Types.ObjectId, ref: "User", required:true},
-    labelColor:{type:String, required:true, default:"#475569"}
-})
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  labelColor: { type: String, required: true, default: "#475569" },
+  banner: {
+    type: String,
+    enum: [
+      "blob",
+      "steps",
+      "circle",
+      "biblioteka",
+      "abstract",
+      "default-banner",
+    ], // Lista dostępnych obrazków
+    required: false,
+    default: "default-banner", // Domyślny obrazek
+  },
+});
 
-const ProductModel = model("Product",productSchema);
+const ProductModel = model("Product", productSchema);
 export default ProductModel;
