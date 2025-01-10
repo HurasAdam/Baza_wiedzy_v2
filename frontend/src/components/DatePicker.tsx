29217b811a78d30dbbd02bef7f43d576fda4db32
HurasAdam <huradam@wp.pl>
2025-01-10 20:20:10 +0100

    feat: Added calendar, DatePicker components.

---
+++ frontend/src/components/DatePicker.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function DatePicker({ state, setState }) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[240px] justify-start text-left font-normal",
+            !state && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon />
+          {state ? format(state, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={state}
+          onSelect={setState}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
--- frontend/src/pages/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage.tsx
@@ -1,67 +1,59 @@
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import { conversationReportApi } from '@/lib/conversationReportsApi'
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { conversationReportApi } from "@/lib/conversationReportsApi";
+import { useQuery } from "@tanstack/react-query";
+import React, { useState } from "react";
 import { TbArrowBadgeRightFilled } from "react-icons/tb";
 import { HiMiniPresentationChartBar } from "react-icons/hi2";
+import { DatePicker } from "@/components/DatePicker";
 
-const StatisticsPage = () => {
-
-const {data:usersWithStats} = useQuery({
-  queryKey:["reportStatistics"],
-  queryFn:()=>{
-    return conversationReportApi.getCoversationReportStats()
-  }
-})
+const StatisticsPage: React.FC = () => {
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
+  const { data: usersWithStats } = useQuery({
+    queryKey: ["reportStatistics"],
+    queryFn: () => {
+      return conversationReportApi.getCoversationReportStats();
+    },
+  });
 
   return (
-    <div className='min-w-[1580px] mx-auto '>
-      <h2 className='flex items-center gap-1.5 text-xl font-semibold mb-10 mt-3'>
-        <HiMiniPresentationChartBar className='w-7 h-7 text-slate-700'/>
-        Statystykki odnotowanych tematów</h2>
+    <div className="min-w-[1580px] mx-auto ">
+      <h2 className="flex items-center gap-1.5 text-xl font-semibold mb-10 mt-3">
+        <HiMiniPresentationChartBar className="w-7 h-7 text-slate-700" />
+        Statystykki odnotowanych tematów
+      </h2>
       <div>
-     { usersWithStats?.map((user,index)=>{
-      return(
-        <Card 
-    
-        className="w-[45%]">
-            <CardHeader className=' py-1'>
-                <CardTitle className='text-sm flex justify-between  '>
-                  <div className='flex items-center gap-x-1'>
-                  <span 
-                
-                    className=' px-1 flex items-center justify-center border border-transparent rounded-lg hover:border hover:broder hover:border-gray-300/90'>
-             
-                {index +1}
-                <TbArrowBadgeRightFilled
-                            
-                            className={"text-gray-600/60"}
-                        />
+        {usersWithStats?.map((user, index) => {
+          return (
+            <Card className="w-[45%]">
+              <CardHeader className=" py-1">
+                <CardTitle className="text-sm flex justify-between  ">
+                  <div className="flex items-center gap-x-1">
+                    <span className=" px-1 flex items-center justify-center border border-transparent rounded-lg hover:border hover:broder hover:border-gray-300/90">
+                      {index + 1}
+                      <TbArrowBadgeRightFilled className={"text-gray-600/60"} />
                     </span>
-            <span>{user.name}</span>
-            <span>{user.surname}</span>
+                    <span>{user.name}</span>
+                    <span>{user.surname}</span>
                   </div>
-                   
-  
+
                   {user.reportCount}
-      
                 </CardTitle>
-            
-              {/* <CardDescription className='text-xs flex items-center gap-1'>
+
+                {/* <CardDescription className='text-xs flex items-center gap-1'>
                             
     
         
                 </CardDescription>  */}
-            </CardHeader>
-    
-       
-           
-        </Card>
-      )
-     })}
+              </CardHeader>
+            </Card>
+          );
+        })}
+        <DatePicker setState={setStartDate} state={startDate} />
+        <DatePicker setState={setEndDate} state={endDate} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatisticsPage
+export default StatisticsPage;
