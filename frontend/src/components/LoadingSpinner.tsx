c168545e18f67275aff60df8c80553669de8822a
https://github.com/HurasAdam <huradam@wp.pl>
2025-01-16 23:32:58 +0100

    feat: Added LoadingSpinner component.

---
+++ frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,13 @@
+const LoadingSpinner = () => {
+    return (
+        <div className='dots-container text-2xl '>
+        <div className='dot '></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+    </div>
+    
+    );
+  };
+  
+  export default LoadingSpinner;
+  
