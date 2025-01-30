7e5f2886fc055cda3d2b63d60c63eae9f6ad642c
https://github.com/HurasAdam <huradam@wp.pl>
2025-01-30 23:47:30 +0100

    feat: Added loading state handling with skeletons in UserReportDetails component.

--- frontend/src/components/UserReportDetails.tsx
+++ frontend/src/components/UserReportDetails.tsx
@@ -4,6 +4,7 @@ import { conversationReportApi } from '@/lib/conversationReportsApi';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import { FaHistory } from "react-icons/fa";
+import Spinner from './core/Spinner';
 
 
 type UserReportDetailsProps = {
@@ -16,19 +17,33 @@ type UserReportDetailsProps = {
 const UserReportDetails:React.FC<UserReportDetailsProps> = ({userId,queryParams}) => {
 
   const { closeContentModal } = useModalContext();
-    const { data: userConversationReports } = useQuery({
+    const { data: userConversationReports,isLoading } = useQuery({
         queryKey: ["userReportStats", userId],
         queryFn: () => {
           return conversationReportApi.getUserConversationReportStats({userId,searchParams:queryParams});
         },
       });
 
+
+
+  
   return (
     <div className='px-2 py-2'>
         <div className='mb-4  flex items-center gap-2 '>
           <FaHistory className='text-sky-700'/>
         <h2 className='font-semibold text-gray-700 '>  Historia odnotowanych rozmów</h2>
           </div>
+          {isLoading ? (
+                 <div className="flex flex-col  h-full space-y-4 ">
+                 <Spinner animation="spin" size="lg" color="bg-blue-500" position='center' />
+               
+                 <div className="w-full space-y-4">
+                   {[...Array(9)].map((_, index) => (
+                     <div key={index} className="h-20 w-full bg-gray-200 rounded-lg " />
+                   ))}
+                 </div>
+               </div>
+          ):
         <ul className="space-y-4">
           {userConversationReports?.length<= 0 ? (        <div className="flex flex-col items-center justify-center p-4 bg-gray-100 border border-gray-200 rounded-lg">
             <div className="w-60 h-60 mb-4">
@@ -86,7 +101,7 @@ const UserReportDetails:React.FC<UserReportDetailsProps> = ({userId,queryParams}
               </div>
             </li>
           ))}
-        </ul>
+        </ul>}
     </div>
   )
 }
