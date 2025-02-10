e44722d5f79f30dba27e4932b8820309bb6e272d
https://github.com/HurasAdam <huradam@wp.pl>
2025-02-10 18:56:55 +0100

    feat: Moved deleteTag logic to repository layer.

--- backend/src/modules/tag/repository/index.ts
+++ backend/src/modules/tag/repository/index.ts
@@ -1,9 +1,10 @@
 import { EHttpCodes } from '../../../enums/http.js';
 import appAssert from '../../../utils/appAssert.js';
-import TagModel from '../model.js';
-import type { ICreateTagParams, IUpdateTagParams } from '../../../types/tag.js';
+import TagModel from '../../tag/model.js';
+import type { GetTagsQuery, GetTagsResponse, ICreateTagParams, IUpdateTagParams } from '../../../types/tag.js';
 import type { ITag } from '../model.js';
 import type express from 'express';
+import ArticleModel from '../../article/models/schema.js';
 export const createTag = async ({ request, userId }: ICreateTagParams): Promise<{ message: string }> => {
   const { name } = request;
 
@@ -26,11 +27,7 @@ export const getTag = async (data: { tagId: string }): Promise<ITag | null> => {
 };
 
 
-interface GetTagsQuery {
-  search?: string;
-}
-
-export const getTags = async({req}:{req:express.Request<GetTagsQuery>})=>{
+export const getTags = async({req}:{req:express.Request<GetTagsQuery>}):Promise<GetTagsResponse>=>{
   const { search = '' } = req.query;
     // Filtr do wyszukiwania tagów
     const filter = {
@@ -83,4 +80,51 @@ export const updateTag = async({request,tagId}:IUpdateTagParams):Promise<{messag
   
     return { message: 'Tag has been updated' };
    
+}
+
+
+export const deleteTag = async({req}:{req:express.Request}):Promise<{message:string}>=>{
+  const { id } = req.params;
+   // Znajdź tag, który próbujesz usunąć
+   const tag = await TagModel.findById(id);
+   appAssert(tag, EHttpCodes.NOT_FOUND, 'Tag not found');
+
+   // Znajdź artykuły, które mają ten tag
+   const articlesWithTag = await ArticleModel.find({ tags: id });
+
+   // Wyszukaj domyślny tag (np. LIBRUS)
+   const defaultTag = await TagModel.findOne({
+     name: 'LIBRUS',
+     isDefault: true,
+   });
+   appAssert(defaultTag, EHttpCodes.NOT_FOUND, 'Domyślny tag nie został znaleziony.');
+
+   // Jeśli artykuł ma tylko jeden tag i jest to tag, który chcemy usunąć
+   for (const article of articlesWithTag) {
+     if (article.tags?.length === 1 && article.tags[0]?.toString() === id) {
+       // Jeśli artykuł ma tylko jeden tag, przypisz domyślny tag i usuń ten tag
+       await ArticleModel.updateOne(
+         { _id: article._id },
+         {
+           $addToSet: { tags: defaultTag._id }, // Dodaj domyślny tag
+         },
+       );
+
+       // Teraz usuń usuwany tag w osobnej operacji
+       await ArticleModel.updateOne(
+         { _id: article._id },
+         { $pull: { tags: id } }, // Usuń usuwany tag
+       );
+     } else {
+       // Jeśli artykuł ma więcej niż jeden tag, po prostu usuń tag
+       await ArticleModel.updateOne(
+         { _id: article._id },
+         { $pull: { tags: id } }, // Usuń usuwany tag
+       );
+     }
+   }
+
+   // Usuń tag z bazy danych
+   await TagModel.findByIdAndDelete(id);
+   return {message:"Tag has been deleted sucessfully"}
 }
--- backend/src/server/modules/tag/remove/index.ts
+++ backend/src/server/modules/tag/remove/index.ts
@@ -1,56 +1,13 @@
+import { deleteTag } from '../../../../modules/tag/repository/index.js';
 import { EHttpCodes } from '../../../../enums/http.js';
-import ArticleModel from '../../../../modules/article/models/schema.js';
-import TagModel from '../../../../modules/tag/model.js';
-import appAssert from '../../../../utils/appAssert.js';
 import catchErrors from '../../../../utils/catchErrors.js';
 import type express from 'express';
 
 export default (): ((req: express.Request, res: express.Response, next: express.NextFunction) => Promise<void>) => {
   return catchErrors(async (req, res) => {
-    const { id } = req.params;
 
-    // Znajdź tag, który próbujesz usunąć
-    const tag = await TagModel.findById(id);
-    appAssert(tag, EHttpCodes.NOT_FOUND, 'Tag not found');
-
-    // Znajdź artykuły, które mają ten tag
-    const articlesWithTag = await ArticleModel.find({ tags: id });
-
-    // Wyszukaj domyślny tag (np. LIBRUS)
-    const defaultTag = await TagModel.findOne({
-      name: 'LIBRUS',
-      isDefault: true,
-    });
-    appAssert(defaultTag, EHttpCodes.NOT_FOUND, 'Domyślny tag nie został znaleziony.');
-
-    // Jeśli artykuł ma tylko jeden tag i jest to tag, który chcemy usunąć
-    for (const article of articlesWithTag) {
-      if (article.tags?.length === 1 && article.tags[0]?.toString() === id) {
-        // Jeśli artykuł ma tylko jeden tag, przypisz domyślny tag i usuń ten tag
-        await ArticleModel.updateOne(
-          { _id: article._id },
-          {
-            $addToSet: { tags: defaultTag._id }, // Dodaj domyślny tag
-          },
-        );
-
-        // Teraz usuń usuwany tag w osobnej operacji
-        await ArticleModel.updateOne(
-          { _id: article._id },
-          { $pull: { tags: id } }, // Usuń usuwany tag
-        );
-      } else {
-        // Jeśli artykuł ma więcej niż jeden tag, po prostu usuń tag
-        await ArticleModel.updateOne(
-          { _id: article._id },
-          { $pull: { tags: id } }, // Usuń usuwany tag
-        );
-      }
-    }
-
-    // Usuń tag z bazy danych
-    await TagModel.findByIdAndDelete(id);
-
-    res.status(EHttpCodes.OK).json({ message: 'Tag został usunięty pomyślnie.' });
+   const {message} = await deleteTag({req})
+   
+    res.status(EHttpCodes.OK).json({ message});
   });
 };
