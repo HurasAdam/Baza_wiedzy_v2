829e85b413c391693400ce03c1bbca6e4b261f6c
https://github.com/HurasAdam <huradam@wp.pl>
2025-02-10 14:10:31 +0100

    feat: Moved getTags logic to the repository layer.

--- backend/src/modules/tag/repository/index.ts
+++ backend/src/modules/tag/repository/index.ts
@@ -3,7 +3,7 @@ import appAssert from '../../../utils/appAssert.js';
 import TagModel from '../model.js';
 import type { ICreateTagParams } from '../../../types/tag.js';
 import type { ITag } from '../model.js';
-
+import type express from 'express';
 export const createTag = async ({ request, userId }: ICreateTagParams): Promise<{ message: string }> => {
   const { name } = request;
 
@@ -24,3 +24,40 @@ export const getTag = async (data: { tagId: string }): Promise<ITag | null> => {
   const tag = await TagModel.findById({ _id: data.tagId });
   return tag;
 };
+
+
+interface GetTagsQuery {
+  search?: string;
+}
+
+export const getTags = async({req}:{req:express.Request<GetTagsQuery>})=>{
+  const { search = '' } = req.query;
+    // Filtr do wyszukiwania tagów
+    const filter = {
+      isDefault: false,
+      ...(search && { name: { $regex: search, $options: 'i' } }), // Wyszukiwanie w nazwach tagów
+    };
+
+    // Agregacja
+    const tags = await TagModel.aggregate([
+      { $match: filter }, // Filtruj tagi
+      {
+        $lookup: {
+          from: 'articles', // Kolekcja, z którą łączymy (nazwa w MongoDB)
+          localField: '_id', // Pole z `TagModel`, które łączymy
+          foreignField: 'tags', // Pole z `ArticleModel`, które łączymy
+          as: 'articleLinks', // Nazwa wynikowej tablicy
+        },
+      },
+      {
+        $addFields: {
+          isUsed: { $gt: [{ $size: '$articleLinks' }, 0] }, // Dodaj pole `isUsed` (true, jeśli powiązania istnieją)
+        },
+      },
+      { $unset: 'articleLinks' }, // Usuń tymczasową tablicę `articleLinks` (opcjonalne)
+    ]);
+
+    // Liczba wszystkich pasujących tagów
+    const totalCount = await TagModel.countDocuments(filter);
+    return{tags,totalCount};
+}
