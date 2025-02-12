71b96a66fb53f8905e8e1302631563798ad74f5d
https://github.com/HurasAdam <huradam@wp.pl>
2025-02-12 01:38:17 +0100

    feat: Enhanced DatePicker component with label props, Added types for component props.

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -13,7 +13,12 @@ import {
 import { PopoverClose } from "@radix-ui/react-popover";
 import { useRef } from "react";
 
-export function DatePicker({ state, setState }) {
+interface IDatePickerProps {
+  state: Date | undefined;
+  setState: (date: Date | undefined) => void;
+  label: string;
+}
+export function DatePicker({ state, setState,label }:IDatePickerProps) {
   const popOverRef = useRef<HTMLButtonElement | null>(null);
 
   return (
@@ -22,12 +27,12 @@ export function DatePicker({ state, setState }) {
         <Button
           variant={"outline"}
           className={cn(
-            "w-[240px] justify-start text-left font-normal",
+            "w-[240px] justify-start text-left font-normal space-x-0.5",
             !state && "text-muted-foreground"
           )}
         >
-          <CalendarIcon />
-          {state ? format(state, "PPP") : <span>Pick a date</span>}
+          <CalendarIcon className=" w-5 h-5"/>
+          {state ? format(state, "PPP") : <span>{label}</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
