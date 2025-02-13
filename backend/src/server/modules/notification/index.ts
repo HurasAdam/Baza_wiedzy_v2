d73aff9871f0690e14db8529fbca5e2ced6e40e8
https://github.com/HurasAdam <huradam@wp.pl>
2025-02-13 21:24:19 +0100

    feat: Added endpoint to mark notifications as read.

--- backend/src/server/modules/notification/index.ts
+++ backend/src/server/modules/notification/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import getUserNotifications from './get/index.js';
+import markNotificationAsRead from './put/index.js';
 
 /**
  * Initialize notifications routes
@@ -9,6 +10,7 @@ export default (): Router => {
   const router = Router();
 
   router.get('/', getUserNotifications());
+  router.put('/:id/read', markNotificationAsRead());
 
   return router;
 };
