eb6d1acae7c193bff7ed91414c27cfce5b61ed69
https://github.com/HurasAdam <huradam@wp.pl>
2025-02-27 23:18:30 +0100

    feat: Added StatisticsPage, updated Navbar and MySidebar components, Added basic theming.

--- frontend/src/components/MySidebar.tsx
+++ frontend/src/components/MySidebar.tsx
@@ -2,29 +2,57 @@ import { Link } from "react-router-dom";
 import { Home, Settings, LogOut } from "lucide-react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { PiArticleMediumFill } from "react-icons/pi";
+import { ImStatsBars2 } from "react-icons/im";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaAddressBook } from "react-icons/fa6";
 
 const MySidebar = () => {
-  const menuItems = [
+  const primaryMenuItems = [
     { icon: <Home size={24} />, label: "Start", link: "/dashboard" },
     { icon: <PiArticleMediumFill size={23} />, label: "Baza Artykułów", link: "/articles" },
+    { icon: <ImStatsBars2 size={23} />, label: "Statystyki", link: "/statistics" },
+    { icon: <FaPhoneSquareAlt size={23} />, label: "Statystyki", link: "/statistics" },
+    { icon: <FaAddressBook size={23} />, label: "Statystyki", link: "/statistics" },
+
+  ];
+
+
+  const utilityMenuItems = [
     { icon: <Settings size={23} />, label: "Ustawienia", link: "/settings" },
     { icon: <LogOut size={23} />, label: "Wyloguj się", link: "/logout" },
-  ];
+
+  ]
 
   return (
     <TooltipProvider>
-      <div className="w-14 min-h-screen bg-white shadow-md flex flex-col items-center py-6 gap-6 border-r">
-        <div className="sticky top-20 ">
-          {menuItems.map((item, index) => (
-            <Tooltip key={index}>
-              <TooltipTrigger asChild>
-                <Link to={item.link} className="block p-2.5 rounded-lg hover:bg-gray-100 transition">
-                  {item.icon}
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent>{item.label}</TooltipContent>
-            </Tooltip>
-          ))}
+      <div className="w-14 min-h-screen bg-background text-foreground shadow-md flex flex-col items-center py-6 gap-6 border-r ">
+        <div className="sticky top-20 flex min-h-screen pb-28 flex-col justify-between">
+          <div className="">
+            {primaryMenuItems.map((item, index) => (
+              <Tooltip key={index}>
+                <TooltipTrigger asChild>
+                  <Link to={item.link} className="block p-2.5 text-foreground rounded-lg transition group-hover:text-foreground">
+                    {item.icon}
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>{item.label}</TooltipContent>
+              </Tooltip>
+            ))}
+          </div>
+          <div>
+            <div className="">
+              {utilityMenuItems.map((item, index) => (
+                <Tooltip key={index}>
+                  <TooltipTrigger asChild>
+                    <Link to={item.link} className="block p-2.5 rounded-lg transition">
+                      {item.icon}
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent>{item.label}</TooltipContent>
+                </Tooltip>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </TooltipProvider>
--- frontend/src/pages/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage.tsx
@@ -1,7 +1,47 @@
+import AButton from "@/components/core/Button"
+import { DatePicker } from "@/components/DatePicker"
+import { useState } from "react"
+import { HiMiniPresentationChartBar } from "react-icons/hi2"
+import { IoMdSearch } from "react-icons/io"
 
 
 export const StatisticsPage = () => {
+
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
+
   return (
-    <div className=" text-slate-700 rounded p-2 h-full bg-white">StatisticsPage</div>
+    <div className="p-5 h-full flex flex-col w-full max-w-[1580px] mx-auto">
+      <h2 className="flex items-center gap-2 text-2xl font-bold mb-8 text-foreground">
+        <HiMiniPresentationChartBar className="w-8 h-8  text-foreground" />
+        Statystyki użytkowników
+      </h2>
+
+      {/* Sekcja filtrów */}
+      <div className="border bg-background  text-foreground rounded-lg p-6 mb-8 shadow-sm">
+        <h3 className="text-lg font-semibold  text-foreground mb-4">Filtruj według zakresu dat</h3>
+        <div className="flex flex-col sm:flex-row items-center gap-4">
+          <DatePicker setState={setStartDate} state={startDate} label="Data początkowa" />
+          <DatePicker setState={setEndDate} state={endDate} label="Data końcowa" />
+          <AButton
+
+            icon={<IoMdSearch className="w-3.5 h-3.5" />}
+            label="Wyszukaj"
+
+            className=""
+          >
+
+          </AButton>
+          <AButton
+            label="   Wyczyść filtry"
+            disabled={!startDate && !endDate}
+
+            className="   "
+          >
+
+          </AButton>
+        </div>
+      </div>
+    </div>
   )
 }
