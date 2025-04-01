49c41bf56d74793d2d52ee772afd04c592f532a0
https://github.com/HurasAdam <huradam@wp.pl>
2025-04-01 21:28:30 +0200

    feat: Added email transporter configuration using Nodemailer for email sending.

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -15,3 +15,6 @@ export const APP_ORIGIN = getEnv("APP_ORIGIN");
 export const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET");
 export const RESEND_API_KEY = getEnv("RESEND_API_KEY");
 export const EMAIL_SENDER = getEnv("EMAIL_SENDER");
+export const EMAIL_SERVICE = getEnv("EMAIL_SERVICE");
+export const EMAIL_SERVICE_HOST = getEnv("EMAIL_SERVICE_HOST");
+export const EMAIL_SENDER_PASSWORD = getEnv("EMAIL_SENDER_PASSWORD");
