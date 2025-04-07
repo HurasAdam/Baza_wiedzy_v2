b59d5f62b29c276d1648ea755bdedddee2e66131
Jan <jbielecki18@gmail.com>
2025-04-07 22:54:47 +0200

    format article dtos, remove comment text

--- backend/src/features/article/dto/search-articles.dto.ts
+++ backend/src/features/article/dto/search-articles.dto.ts
@@ -3,9 +3,13 @@ import { searchDto } from "@/common/dto/search.dto";
 
 export const searchArticlesDto = searchDto.extend({
     title: z.string().optional(),
+
     tags: z.union([z.array(z.string()), z.string()]).optional(),
+
     product: z.string().optional(),
+
     author: z.string().optional(),
+
     verified: z.string().optional(),
 });
 
