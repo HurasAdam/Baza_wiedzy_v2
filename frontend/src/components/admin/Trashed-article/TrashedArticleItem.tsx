bbc6e256f12c5c24cd7b3baa6734e2adec73bfd0
HurasAdam <huradam@wp.pl>
2025-05-01 00:55:27 +0200

    feat: Enhanced restore and remove articles handling with required confiration dialog.

--- frontend/src/components/admin/Trashed-article/TrashedArticleItem.tsx
+++ frontend/src/components/admin/Trashed-article/TrashedArticleItem.tsx
@@ -7,35 +7,66 @@ import toast from "react-hot-toast";
 import { Trash2, CornerUpLeft } from "lucide-react";
 import TrashedArticleDetailsContainer from "./TrashedArticleDetailsContainer";
 import { IArticle } from "@/types";
+import { Alert } from "@/components/alert/Alert";
+import { useAlert } from "@/components/alert/hooks/useAlert";
 
 export const TrashedArticleItem = ({ article, className }: { article: IArticle; className?: string }) => {
     const queryClient = useQueryClient();
     const { openModal, isOpen, closeModal } = useModal();
+    const {
+        isOpen: isRestoreArticleAlertOpen,
+        openAlert: openRestoreArticleAlert,
+        closeAlert: closeRestoreArticleAlert,
+    } = useAlert();
 
-    const { mutate: restore } = useMutation({
+    const {
+        isOpen: isRemoveArticleAlertOpen,
+        openAlert: openRemoveArticleAlert,
+        closeAlert: closeRemoveArticleAlert,
+    } = useAlert();
+
+    const { mutate: restoreArtcileMutation } = useMutation({
         mutationFn: (id: string) => articleApi.restoreArticle({ id }),
         onSuccess: () => {
             queryClient.invalidateQueries(["trashed-articles"]);
-            toast.success("Artykuł przywrócony z kosza");
+            toast.success("Artykuł został przywrócony z kosza");
         },
     });
 
-    const { mutate: remove } = useMutation({
-        mutationFn: (id: string) => articleApi.permanentlyDeleteArticle({ id }),
+    const { mutate: removeArticleMutation } = useMutation({
+        mutationFn: (id: string) => articleApi.deleteArticle({ id }),
         onSuccess: () => {
             queryClient.invalidateQueries(["trashed-articles"]);
-            toast.success("Artykuł trwale usunięty");
+            toast.success("Artykuł został usunięty");
         },
     });
 
     const handleRestore = (e: React.MouseEvent) => {
         e.stopPropagation();
-        restore(article._id);
+        restoreArtcileMutation(article._id);
     };
 
     const handleDelete = (e: React.MouseEvent) => {
         e.stopPropagation();
-        remove(article._id);
+        removeArticleMutation(article._id);
+    };
+
+    const openRestoreAlert = (e) => {
+        e.stopPropagation();
+        openRestoreArticleAlert();
+    };
+
+    const openRemoveAlert = (e) => {
+        e.stopPropagation();
+        openRemoveArticleAlert();
+    };
+
+    const onRemoveConfirm = (id) => {
+        removeArticleMutation(id);
+    };
+
+    const onRestoreConfirm = (id) => {
+        restoreArtcileMutation(id);
     };
 
     return (
@@ -49,14 +80,14 @@ export const TrashedArticleItem = ({ article, className }: { article: IArticle; 
                         <h3 className="text-lg font-semibold text-foreground line-clamp-2">{article.title}</h3>
                         <div className="absolute top-8 right-4 flex space-x-2  group-hover:opacity-100 transition-opacity">
                             <button
-                                onClick={handleRestore}
+                                onClick={(e) => openRestoreAlert(e)}
                                 className="p-2 bg-card/80 rounded-lg shadow hover:shadow-md transition-colors group  hover:bg-muted/50"
                                 aria-label="Przywróć z kosza"
                             >
                                 <CornerUpLeft className="h-4 w-4 text-primary group-hover:text-emerald-400" />
                             </button>
                             <button
-                                onClick={handleDelete}
+                                onClick={(e) => openRemoveAlert(e)}
                                 className="p-2 bg-card/80 rounded-lg shadow hover:shadow-md transition-colors group  hover:bg-muted/50"
                                 aria-label="Usuń na stałe"
                             >
@@ -83,6 +114,23 @@ export const TrashedArticleItem = ({ article, className }: { article: IArticle; 
             <Modal isOpen={isOpen} onClose={closeModal}>
                 <TrashedArticleDetailsContainer productId={article?._id} />
             </Modal>
+            <Alert
+                onConfirm={() => onRemoveConfirm(article?._id)}
+                requireConfirmation={true}
+                isOpen={isRemoveArticleAlertOpen}
+                onCancel={closeRemoveArticleAlert}
+            >
+                <div>Czy jesteś pewien że chcesz trwale usunąć ten artykuł?</div>
+            </Alert>
+
+            <Alert
+                onConfirm={() => onRestoreConfirm(article?._id)}
+                requireConfirmation={true}
+                isOpen={isRestoreArticleAlertOpen}
+                onCancel={closeRestoreArticleAlert}
+            >
+                <div>Czy jesteś pewien że chcesz przywrócić ten artykuł?</div>
+            </Alert>
         </div>
     );
 };
