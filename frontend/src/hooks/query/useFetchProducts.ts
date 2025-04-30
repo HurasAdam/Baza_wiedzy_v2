716247e69502125e1b29aef26694235a2e921d9d
HurasAdam <huradam@wp.pl>
2025-05-01 00:53:56 +0200

    refactor: update useFetchProducts to support dynamic query params.

--- frontend/src/hooks/query/useFetchProducts.ts
+++ frontend/src/hooks/query/useFetchProducts.ts
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { productApi } from "@/lib/product.api";
 
-export const useFetchProducts = () => {
+export const useFetchProducts = (params?: URLSearchParams) => {
     const { data, isError, error } = useQuery({
         queryKey: ["products"],
-        queryFn: productApi.find,
+        queryFn: () => {
+            return productApi.find(params);
+        },
         staleTime: 1000 * 60 * 5,
     });
 
