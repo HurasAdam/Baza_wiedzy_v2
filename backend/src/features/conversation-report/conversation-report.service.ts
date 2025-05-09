bf54b4749158b95e54fe6b115189e8adc649a97e
HurasAdam <huradam@wp.pl>
2025-05-09 21:01:55 +0200

    feat: Add support for report type in creation request.

--- backend/src/features/conversation-report/conversation-report.service.ts
+++ backend/src/features/conversation-report/conversation-report.service.ts
@@ -7,6 +7,7 @@ import appAssert from "../../utils/appAssert";
 interface CreateConversationTopicRequest {
     description?: string;
     topic: string;
+    type: string;
 }
 
 interface CreateConversationTopicParams {
@@ -15,13 +16,14 @@ interface CreateConversationTopicParams {
 }
 
 export const addConversationReport = async ({ request, userId }: CreateConversationTopicParams) => {
-    const { topic, description } = request;
+    const { topic, description, type } = request;
 
     const conversationTopic = await ConversationTopicModel.exists({ topic });
     appAssert(!conversationTopic, CONFLICT, "Conversation topic does not exist");
 
     const createdConversationTopic = await ConversationReportModel.create({
         description,
+        type,
         createdBy: userId,
         topic,
     });
