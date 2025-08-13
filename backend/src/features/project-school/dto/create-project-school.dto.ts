e75e81b3737ab0c6ff5be5cbff40998c2c925704
https://github.com/HurasAdam <huradam@wp.pl>
2025-08-13 02:05:04 +0200

    fix: remove max length constraint from phone field in createSchoolDto validation

--- backend/src/features/project-school/dto/create-project-school.dto.ts
+++ backend/src/features/project-school/dto/create-project-school.dto.ts
@@ -4,7 +4,7 @@ export const createSchoolDto = z.object({
     email: emailSchema,
     name: z.string().min(3).max(255),
     adres: z.string().min(3).max(255),
-    phone: z.string().min(2).max(12),
+    phone: z.string().min(2),
     // project: z.string(),
     szId: z.string().min(1),
 });
