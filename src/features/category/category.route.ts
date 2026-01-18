13e973e6a0619907c95144dab04b42d2cdb2950d
HurasAdam <huradam@wp.pl>
2026-01-18 20:12:08 +0100

    feat:add changelog module

--- src/features/category/category.route.ts
+++ src/features/category/category.route.ts
@@ -13,15 +13,3 @@ categoryRoutes.get("/product/:id", categoryController.findByProduct);
 categoryRoutes.post("/:id/create", categoryController.create);
 categoryRoutes.put("/:id", categoryController.updateOne);
 categoryRoutes.delete("/:id", categoryController.deleteOne);
-
-// -----------------------
-// maybe in future
-// -----------------------
-// export const categoryRoutes = routing((route) => {
-//     route.get("/", categoryController.find);
-//     route.get("/:id", categoryController.findOne);
-//     route.get("/product/:id", categoryController.findByProduct);
-//     route.post("/", categoryController.create);
-//     route.put("/:id", categoryController.updateOne);
-//     route.delete("/:id", categoryController.deleteOne);
-// });
