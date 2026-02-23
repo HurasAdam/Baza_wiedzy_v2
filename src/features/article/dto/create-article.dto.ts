f0dc4c1b82cee484c96e230c9fe7a90c20890542
HurasAdam <huradam@wp.pl>
2026-02-23 20:03:44 +0100

    fix(create-article-dto): unify title min length validation to 3 chars

--- src/features/article/dto/create-article.dto.ts
+++ src/features/article/dto/create-article.dto.ts
@@ -2,7 +2,7 @@ import { Types } from "mongoose";
 import { z } from "zod";
 
 export const createArticleDto = z.object({
-    title: z.string().trim().min(4).max(255),
+    title: z.string().trim().min(3).max(255),
     employeeDescription: z.string().trim().min(1).max(9000),
     tags: z.array(z.string().refine(Types.ObjectId.isValid)).nonempty(),
     product: z.string().refine(Types.ObjectId.isValid),
